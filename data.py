d9f76087ef4a650f7ddc3ea7c3fa835946921fc5
olgadoronina <olgadorr@gmail.com>
2018-04-21 17:46:17 -0600

    Added ability to pick filters and noise.

--- data.py
+++ data.py
@@ -24,7 +24,16 @@ dx_coarse = np.divide([np.pi, np.pi], [256, 256])
 # n_sets = 4  # number of subsets for training and test data
 
 
-def load_data(dimension = 2):
+def load_data(dimension=2):
+    """
+    Load data from either .csv or .npz file.
+    If you already have the isotropic2048.npz file in your directory,
+    use this file to load the data (it is more efficient, do this by using the line filename_cvs = None and
+    COMMENTING OUT filename_cvs = '../data/isotropic2048.csv.
+    Function can load 3d data as well as 2d. 3d fine data is 256*256*256.
+    :param dimension: 2 or 3
+    :return: dictionary of 'u', 'v', 'w' data array (256^3 for 3d and 2048^2 for 2d)
+    """
     logging.info('Fine data')
     velocity = dict()
     if dimension == 2:
@@ -80,11 +89,13 @@ def example_of_data(velocity, Npoints_coarse=256):
     plotting.spectra(plot_folder, plot_folder+'spectra', '')
 
 
-def form_train_test_sets(velocity, Npoints_coarse=256):
+def form_train_test_sets(velocity, Npoints_coarse=256, filter_type='gaussian'):
     """
     Implement the shifting strategy and return 4 data structures:
     data_train, data_test, filtered_train, filtered_test.
-    :param velocity:  velocity dictionary (where each key `u, v, w` corresponds to a 2048x2048 array)
+    :param velocity:  velocity dictionary
+    :param Npoints_coarse: number of points in coarse array (should be 256 in 2d and 64 in 3d)
+    (where each key `u, v, w` corresponds to a 2048^2 array for 2d and 256^3 for 3d)
     :return:
     """
     logging.info('Creating training and test data sets')
@@ -103,14 +114,36 @@ def form_train_test_sets(velocity, Npoints_coarse=256):
     for i in ind[1:]:       # use other to random numbers to draw test sets
         data_test.append(utils.sparse_dict(velocity, Npoints_coarse, i))
 
+    # Filtering
     filtered_train = dict()
-    for key, value in data_train.items():
-        filtered_train[key] = ndimage.gaussian_filter(value, sigma=1,  mode='wrap', truncate=500)
-
     filtered_test = [dict(), dict(), dict()]
-    sigma = [1, 1.1, 0.9]
-    for i in range(3):
-        for key, value in data_test[i].items():
-            filtered_test[i][key] = ndimage.gaussian_filter(value, sigma=sigma[i], mode='wrap', truncate=500)
+
+    if filter_type == 'gaussian':
+        for key, value in data_train.items():
+            filtered_train[key] = ndimage.gaussian_filter(value, sigma=1,  mode='wrap', truncate=500)
+        sigma = [1, 1.1, 0.9]
+        for i in range(3):
+            for key, value in data_test[i].items():
+                filtered_test[i][key] = ndimage.gaussian_filter(value, sigma=sigma[i], mode='wrap', truncate=500)
+
+    elif filter_type == 'median':
+        for key, value in data_train.items():
+            filtered_train[key] = ndimage.median_filter(value, size=10,  mode='wrap')
+        s = [10, 13, 7]
+        for i in range(3):
+            for key, value in data_test[i].items():
+                filtered_test[i][key] = ndimage.median_filter(value, size=s, mode='wrap')
+
+    elif filter_type == 'noise':
+        for key, value in data_train.items():
+            kappa = np.random.normal(0, 1, size=value.shape)
+            filtered_train[key] = value + 0.2*kappa
+        mu = [0.2, 0.22, 0.18]
+        for i in range(3):
+            for key, value in data_test[i].items():
+                kappa = np.random.normal(0, 1, size=value.shape)
+                filtered_test[i][key] = value + mu[i]*kappa
+    else:
+        logging.error('Filter type is not defined.')
 
     return filtered_train, data_train, filtered_test, data_test
